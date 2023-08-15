d2d5f1354fd96336e0d502a10aec3bd8b6f0fa32
lawrence1986 <madusquare@gmail.com>
2023-08-15 09:05:56 -0700

    Updates on task 5 and 6

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- This prints the full description of first_table from the database called hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+-- DESCRIBE or EXPLAIN statements NOT ALLOWED
+
+SHOW CREATE TABLE first_table;
