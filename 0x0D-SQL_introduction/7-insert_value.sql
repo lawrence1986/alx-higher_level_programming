cdca2ba41c7898d90e03391f469650adcc6828de
lawrence1986 <madusquare@gmail.com>
2023-08-15 09:16:41 -0700

    Updates for 7 & 8 tasks

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+-- Insert a new row into the table first_table in the hbtn_0c_0 database
+-- New row:
+--     id = 89
+--     name = Best School
+-- The database name will be passed as an argument of the mysql command
+
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Displays the number of records with id = 89 in first_table of the db hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT COUNT(*) 
+	FROM first_table 
+	WHERE id=89;
