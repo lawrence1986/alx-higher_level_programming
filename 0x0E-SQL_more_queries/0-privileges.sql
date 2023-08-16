673c925856d216fbe3c3dbade8a98400ab44a99b
lawrence1986 <madusquare@gmail.com>
2023-08-16 09:23:29 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Simply lists all privileges of the MySQL users user_0d_1 and user_0d_2 on server.
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
